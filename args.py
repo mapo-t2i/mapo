4f2905fca3d82b2f3b0a74c0c5ecb0ca42bee260
sayakpaul <spsayakpaul@gmail.com>
2024-06-06 17:44:14 +0530

    add lora support

--- args.py
+++ args.py
@@ -263,6 +263,12 @@ def parse_args(input_args=None):
     )
     parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank")
     parser.add_argument(
+        "--lora_rank",
+        type=int,
+        default=None,
+        help=("The dimension of the LoRA update matrices."),
+    )
+    parser.add_argument(
         "--enable_xformers_memory_efficient_attention", action="store_true", help="Whether or not to use xformers."
     )
     parser.add_argument(
